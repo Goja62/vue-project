7a25dbcb0e744f6182c97f7abf774d946977db7b
Goja <gortnar@beograd.com>
2025-01-12 14:23:20 +0100

    Creating a Basic Vue App

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'goja',
+          name: 'Zoran Gortnar',
+          phone: '2101914',
+          email: 'gortnar@beograd.com',
+        },
+        {
+          id: 'jaca',
+          name: 'Jasna Knežević',
+          phone: '2493112',
+          email: 'jasnakn@beograd.com',
+        },
+      ],
+    };
+  },
+};
+</script>
